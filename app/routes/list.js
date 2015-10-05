3a5e8acdc3784641932ef6bf104b8a8bb83edf80
nicholasH <nicholas.hoover@hope.edu>
2015-10-05 17:45:15 +0000

    Trying to fix the error uncauqht error: could not find module 'todoList/models/todo' imported from 'todo-list/routes/list'

--- app/routes/list.js
+++ app/routes/list.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import Todo from 'todoList/models/todo';
+
 
 export default Ember.Route.extend({
+    model: function() {
+
+
+   return this.store.findAll('todo');
+    }
 });
